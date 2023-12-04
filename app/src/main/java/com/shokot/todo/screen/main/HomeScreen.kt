526134dff5f6d2be952a52ed288df79e261a6f0f
Md Shokot Alam <mdshokot.alam@studio.unibo.it>
2023-12-04 20:44:38 +0100

    ToDo: - add drop down menu to Home screen

--- app/src/main/java/com/shokot/todo/screen/main/HomeScreen.kt
+++ app/src/main/java/com/shokot/todo/screen/main/HomeScreen.kt
@@ -1,21 +1,38 @@
 package com.shokot.todo.screen.main
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.shokot.todo.R
+import com.shokot.todo.screen.main.components.home.DropDownMenu
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Home Screen",
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleMedium
-        )
+    val defaultSelected = stringResource(id = R.string.all)
+    var selectedItem by rememberSaveable {
+        mutableStateOf(defaultSelected)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        DropDownMenu(selectedItem = selectedItem, onSelectedItemChange = { selectedItem = it })
+        Spacer(modifier = Modifier.height(5.dp))
+
     }
 }
+
+
+
